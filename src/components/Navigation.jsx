373a17d229a8b0f60bb58203bca66e4d2293e56c
lostNIII3 <73011138+NitinSirsath@users.noreply.github.com>
2022-05-18 02:51:46 +0530

    initial release

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { List } from '../styles/NavbarStyle'
+
+const Navigation = () => {
+  return (
+    <div>
+        {/* <Link to='/'>Home</Link>
+        <Link to='/about'>About</Link>
+        <Link to='/contact'>Contact</Link> */}
+        <nav className="px-3 navbar navbar-expand-lg bg-light">
+  <div className="container-fluid">
+    <Link className="navbar-brand" to='/'>CRUD APPLICATION</Link>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+        <List className="nav-item">
+        <Link to='/'>Home</Link>
+        </List>
+        <List className="nav-item">
+        <Link to='/about'>About</Link>
+        </List>
+        <List className="nav-item">
+        <Link to='/contact'>Contact</Link>
+        </List>
+       
+      </ul>
+    </div>
+  </div>
+</nav>
+    </div>
+  )
+}
+
+export default Navigation
---
+++ src/styles/NavbarStyle.jsx
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const List = styled.li`
+ list-style: none ;
+ padding: 0px 20px;
+`
